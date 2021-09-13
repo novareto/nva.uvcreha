0b8e64fecc97be72946b60604b859c5eb7d13da3
Souheil Chelfouh <trollfot@gmail.com>
2021-09-13 15:40:00 +0200

    Running app

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import bjoern
 import fanstatic
 import pathlib
+import importscan
 import uvcreha.app
+import uvcreha.browser
 import uvcreha.auth
 import uvcreha.emailer
+import uvcreha.auth.utilities
+import reiter.view.utils
 
 from reha.prototypes.workflows.user import user_workflow
 from reiter.application.app import BrowserApplication
+from reiter.events.meta import Subscribers
+from roughrider.routing.route import NamedRoutes
 from uvcreha import plugins
 from uvcreha.auth import filters as auth_filters
+from uvcreha.request import Request
 
 
 ### Middlewares
@@ -22,14 +29,38 @@ session = plugins.session_middleware(
 )
 
 # authentication
+class User(uvcreha.auth.User):
+
+    id: str
+
+    def __init__(self, login):
+        self.id = login
+
+
+class Source:
+
+    _users = {
+        'admin': "admin"
+    }
+
+    def find(self, credentials: dict):
+        if credentials['login'] in self._users:
+            if self._users[credentials['login']] == credentials['password']:
+                return User(credentials['login'])
+
+    def fetch(self, loginname):
+        if loginname in self._users:
+            return User(loginname)
 
 
 authentication = uvcreha.auth.Auth(
     user_key="test.principal",
     session_key=session.environ_key,
-    sources=None,
+    sources=[Source()],
     filters=(
-        auth_filters.security_bypass("/login"),
+        auth_filters.security_bypass([
+            "/login"
+        ]),
         auth_filters.secured(path="/login"),
         auth_filters.filter_user_state(states=(
             user_workflow.states.inactive,
@@ -37,15 +68,6 @@ authentication = uvcreha.auth.Auth(
         )),
         auth_filters.TwoFA(path="/2FA")
     )
-)
-
-
-# Static assets
-assets = fanstatic.Fanstatic(
-    compile=True,
-    recompute_hashes=True,
-    bottom=True,
-    publisher_signature="static"
 )
 
 
@@ -69,7 +91,7 @@ webpush = plugins.webpush_plugin(
 
 # Email
 emailer = uvcreha.emailer.SecureMailer(
-    user=None
+    user=None,
     password=None,
     emitter="uvcreha@novareto.de"
 )
@@ -81,8 +103,10 @@ twoFA = uvcreha.auth.utilities.TwoFA(
 
 
 # Application
-uvcreha = BrowserApplication(
+app = BrowserApplication(
     ui=uvcreha.app.ui,
+    routes=NamedRoutes(extractor=reiter.view.utils.routables),
+    request_factory=Request,
     utilities={
         "webpush": webpush,
         "emailer": emailer,
@@ -93,20 +117,29 @@ uvcreha = BrowserApplication(
 )
 
 
-uvcreha.route(
+# my routes
+import uvcreha.browser.login
+
+app.routes.register('/login')(uvcreha.browser.login.LoginForm)
 
 
+
+importscan.scan(uvcreha.browser)
 
 # Run me
 bjoern.run(
     host="0.0.0.0",
-    port="8080",
+    port=8080,
     reuse_port=True,
-    wsgi_app=assets(
+    wsgi_app=fanstatic.Fanstatic(
         session(
             authentication(
-                uvcreha
+                app
             )
-        )
+        ),
+        compile=True,
+        recompute_hashes=True,
+        bottom=True,
+        publisher_signature="static"
     )
 )
